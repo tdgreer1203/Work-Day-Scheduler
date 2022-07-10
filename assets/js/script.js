63c5182436394855d58d99e4076be44b0badaf55
Theo <tdgreer1203@gmail.com>
2022-07-10 07:13:50 -0400

    Getting Help with DIV click function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 var eventsArray = [];
 
+//Sets the date in the heading
 $('#currentDay').text(moment().format('dddd, MMMM Do'));
 
+//This is just the funciton to save the tasks. 
+function saveTasks() {
+    localStorage.setItem("events", JSON.stringify(eventsArray));
+}
+
+//This creates the layout of the screen when the application is started
+//Later on, I will include the ability to load saved events
 function createDay() {
     var container = $('.container');
     var time = moment().hours(8).format('hA');
@@ -17,6 +25,14 @@ function createDay() {
     }
 }
 
+//Replaces the actual description area with a text area field to add/update an event
+$("div.description").on("click", function(event) {
+    console.log(event);
+    var text = $(this).text().trim();
+    var textInput = $("<textarea>").val(text);
 
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+});
 
 createDay();
